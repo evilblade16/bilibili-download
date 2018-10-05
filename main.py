a7ada9156bf6d1ccda6dc5eff13222dc678b485f
Hellowshuo <vmluaij18@163.com>
2018-10-05 12:50:27 +0000

    version 1

---
+++ main.py
@@ -0,0 +1,355 @@
+# https://www.bilibili.com/video/av15478453
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWebEngineWidgets import *
+import sys,os
+from ui import *
+from ui2 import *
+import time,pyperclip
+import re
+import ssl
+import urllib.request
+import requests
+import threading
+import qdarkstyle
+qdarkstyle.load_stylesheet_pyqt5
+from concurrent.futures import ThreadPoolExecutor
+from collections import Counter 
+headers = {
+"User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.26 Safari/537.36 Core/1.63.6726.400 QQBrowser/10.2.2265.400",
+"Cookie":"CURRENT_QUALITY=64; DedeUserID=227050458; "
+}
+
+
+
+class MyMainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(MyMainWindow, self).__init__()
+        self.setupUi(self)
+        # self.show_web()
+        self.setWindowFlag(Qt.FramelessWindowHint)
+        self.display_table.verticalScrollBar().setStyleSheet(
+        "QScrollBar:vertical{"        #垂直滑块整体  
+          "padding-top:20px;"    #上预留位置（放置向上箭头）  
+          "padding-bottom:20px;" #下预留位置（放置向下箭头）  
+          "padding-left:3px;"    #左预留位置（美观）  
+          "padding-right:3px;"   #右预留位置（美观）  
+          "border-left:1px solid white;}"#左分割线  
+          "QScrollBar::handle:vertical{"#滑块样式  
+          "background:rgba(100,100,100,0.3);"  #滑块颜色  
+          "border-radius:6px;"   #边角圆润  
+          "min-height:60px;}"    #滑块最小高度  
+          "QScrollBar::handle:vertical:hover{"#鼠标触及滑块样式  
+          "background:rgba(0,0,0,0.3);}" #滑块颜色  
+          "QScrollBar::add-line:vertical{"#向下箭头样式  
+          "background:;}"  
+          "QScrollBar::sub-line:vertical{"#向上箭头样式  
+          "background:;}") 
+        self.display_table.horizontalScrollBar().setStyleSheet(
+        "QScrollBar:vertical{"        #垂直滑块整体  
+          "padding-top:20px;"    #上预留位置（放置向上箭头）  
+          "padding-bottom:20px;" #下预留位置（放置向下箭头）  
+          "padding-left:3px;"    #左预留位置（美观）  
+          "padding-right:3px;"   #右预留位置（美观）  
+          "border-left:1px solid white;}"#左分割线  
+          "QScrollBar::handle:vertical{"#滑块样式  
+          "background:rgba(100,100,100,0.3);"  #滑块颜色  
+          "border-radius:6px;"   #边角圆润  
+          "min-height:60px;}"    #滑块最小高度  
+          "QScrollBar::handle:vertical:hover{"#鼠标触及滑块样式  
+          "background:rgba(0,0,0,0.3);}" #滑块颜色  
+          "QScrollBar::add-line:vertical{"#向下箭头样式  
+          "background:;}"  
+          "QScrollBar::sub-line:vertical{"#向上箭头样式  
+          "background:;}")
+        self.display_table.horizontalHeader().setStyleSheet(
+            "QHeaderView::section{background:rgba(0,0,0,0.1); padding-left:4px; border:1px solid white; }"
+            # "QHeaderView::section:checked{background-color:green; }"
+            )
+        self.display_table.verticalHeader().setStyleSheet(
+            "QHeaderView::section{background:rgba(0,0,0,0.1); padding-left:4px; border:1px solid white; }"
+            # "QHeaderView::section:checked{background-color:green; }"
+            )
+        self.display_table.setStyleSheet('''
+
+            ''')
+        self.run_dir = os.getcwd()
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.input_url)
+        self.timer.start(1000)
+        self.display_table.setColumnWidth(0,60)
+        self.display_table.setColumnWidth(1,250)
+        self.display_table.setColumnWidth(2,70)
+        self.display_table.setColumnWidth(3,70)
+        self.display_table.setColumnWidth(4,250)
+        self.display_table.setColumnWidth(5,100)
+        self.workjiexi = Workjiexi()
+        self.workjiexi.done.connect(self.jiexi_done)
+        self.workjiexi.finished.connect(self.jiexi_finshed)
+        self.workdownload = Workdownload()
+        self.workdownload.update.connect(self.update_progress)
+        self.workdownload.para_update.connect(self.para_done)
+
+        self.list_ = []
+        self.title = ''
+
+    def mousePressEvent(self, event):
+        x = (event.globalPos()-self.pos()).x()
+        y = (event.globalPos()-self.pos()).y()
+        if event.button()==Qt.LeftButton and x<915 and y<40:
+            self.m_flag=True
+            self.m_Position=event.globalPos()-self.pos() #获取鼠标相对窗口的位置
+            event.accept()
+            self.setCursor(QCursor(Qt.OpenHandCursor))
+    
+    def mouseMoveEvent(self, QMouseEvent):
+        if Qt.LeftButton and self.m_flag:  
+            self.move(QMouseEvent.globalPos()-self.m_Position)#更改窗口位置
+            QMouseEvent.accept()
+            
+    def mouseReleaseEvent(self, QMouseEvent):
+        self.m_flag=False
+        self.setCursor(QCursor(Qt.ArrowCursor))
+
+
+    def input_url(self):
+        if re.match(r'^https://www.bilibili.com/video/av',pyperclip.paste()):
+            if self.url_edit.text() == "":
+                self.url_edit.setText(pyperclip.paste())    
+
+    def show_web(self):
+        self.dialog = QDialog()
+        self.hlayout = QHBoxLayout()
+        self.browser = MyWebEngineView()
+        self.browser.load(QUrl('https://passport.bilibili.com/login'))
+        self.hlayout.addWidget(self.browser)
+        self.dialog.setLayout(self.hlayout)
+        self.dialog.setWindowTitle("登录")
+        self.dialog.setWindowModality(Qt.ApplicationModal)
+        self.dialog.resize(500,500)
+        self.dialog.show()
+
+    def jiexi(self):
+        
+        self.list_ = []
+        while self.display_table.rowCount():
+            self.display_table.removeRow(0)
+        if self.url_edit.text():
+            self.workjiexi.url = self.url_edit.text()
+            # self.workjiexi.url = "https://www.bilibili.com/video/av15478453"
+            self.workjiexi.start()
+
+    def jiexi_done(self,list_,title):
+        self.title = title
+        self.list_.append(list_)
+        
+        rowcount = self.display_table.rowCount()
+        # if rowcount != 0:
+        #     if int(list_[0]) >= rowcount:
+        #         self.insert_table(rowcount,list_)
+        #     else:
+        #         for i in range(rowcount):
+        #             if int(list_[0]) < int(self.display_table.item(i,1).text().split('.')[0]):
+        #                 self.insert_table(i,list_)
+        #                 break
+
+        # else:
+        self.insert_table(rowcount,list_)
+        # self.display_table.setItem(rowcount,3,content)
+    def insert_table(self,index,list_):
+        check_radio = QCheckBox()
+        progress = QProgressBar()
+        self.display_table.insertRow(index)
+        self.display_table.setCellWidget(index,0,check_radio)
+        self.display_table.setItem(index,1,QTableWidgetItem(list_[1]))
+        self.display_table.setItem(index,2,QTableWidgetItem(list_[2]))
+        self.display_table.setItem(index,3,QTableWidgetItem(str(list_[3])+" M"))
+        self.display_table.setCellWidget(index,4,progress)
+        self.display_table.setItem(index,5,QTableWidgetItem("0/"+str(len(list_[4]))))
+
+    def jiexi_finshed(self):
+        self.all_select_check_button.animation = QPropertyAnimation(self.all_select_check_button, b'geometry')
+        self.all_select_check_button.animation.setDuration(1000)
+        self.all_select_check_button.animation.setStartValue(QRect(-150, 100,  121, 21))
+        self.all_select_check_button.animation.setEndValue(QRect(20, 100, 121, 21))
+        self.all_select_check_button.animation.start()
+
+        self.download_button.animation = QPropertyAnimation(self.download_button, b'geometry')
+        self.download_button.animation.setDuration(1000)
+        self.download_button.animation.setStartValue(QRect(922,50, 93, 41))
+        self.download_button.animation.setEndValue(QRect(780, 50, 93, 41))
+        self.download_button.animation.start()
+
+    def download(self):
+        if not os.path.exists(self.title):
+            os.makedirs(self.title)
+        self.workdownload.num = []
+        self.workdownload.title = self.title
+        self.workdownload.list_ = self.list_
+        for i in range(self.display_table.rowCount()):
+            if self.display_table.cellWidget(i,0).isChecked():
+                self.workdownload.num.append(i)
+        self.workdownload.start()
+        
+    def all_select(self,v):
+        for i in range(self.display_table.rowCount()):
+            self.display_table.cellWidget(i,0).setChecked(v)
+
+    def update_progress(self,percent,num):
+        self.display_table.cellWidget(num,4).setValue(percent)
+
+    def para_done(self,num,para):
+        self.display_table.setItem(num,5,QTableWidgetItem(para+'/'+self.display_table.item(num,5).text().split('/')[1]))
+
+    def close_click(self):
+        self.close()
+
+    def min_click(self):
+        self.showMinimized()
+  
+
+    def conver_done(self):
+        QMessageBox.information(self,'提示','转换成功！！')
+
+class MyWebEngineView(QWebEngineView):
+    def __init__(self, *args, **kwargs):
+        super(MyWebEngineView, self).__init__(*args, **kwargs)
+        # 绑定cookie被添加的信号槽
+        QWebEngineProfile.defaultProfile().cookieStore().cookieAdded.connect(self.onCookieAdd)
+        self.cookies = {}       # 存放cookie字典
+        self.result_cookie = ""
+        self.cookie_true = True
+
+    def onCookieAdd(self, cookie):                       # 处理cookie添加的事件
+        name = cookie.name().data().decode('utf-8')     # 先获取cookie的名字，再把编码处理一下
+        value = cookie.value().data().decode('utf-8')   # 先获取cookie值，再把编码处理一下
+        self.cookies[name] = value                       # 将cookie保存到字典里
+        cookie = self.cookies.get('DedeUserID','')
+        if  cookie:
+            if self.cookie_true:
+                self.cookie_true = False
+                self.result_cookie = cookie
+
+class Workjiexi(QThread):
+    """docstring for WorkThread"""
+    done = pyqtSignal(list,str)
+    all_done = pyqtSignal()
+    error = pyqtSignal()
+    def __init__(self):
+        super(Workjiexi, self).__init__()
+    def run(self):        
+        executor = ThreadPoolExecutor(50)
+        result = requests.get(self.url,headers=headers).text
+        title_zz = '<h1 title="(.*?)">'
+        self.title = re.findall(title_zz,result)[0].replace(' ','-')
+        zz = 'page":(.*?),"from":"vupload","part":"(.*?)","duration":'
+        executor.map(self.thread_jiexi,[i[0] for i in re.findall(zz,result)],[i[1] for i in re.findall(zz,result)])
+
+
+
+    def thread_jiexi(self,page,title):
+        url2 = self.url + '?p=' +str(page)
+        respond2 = requests.get(url2,headers=headers).text
+        zz2 = '"order":(.*?),"length":(.*?),"size":(.*?),.*?"url":"(.*?)"'
+        result2 = re.findall(zz2,respond2)
+        time =str(sum([int(i[1]) for i in result2])//60000) +":"+str(int((sum([int(i[1]) for i in result2])%60000)//1000))
+        size =round (sum([int(i[2]) for i in result2])/1024/1024,1)
+        url = [i[3] for i in result2]
+        
+        result3 = [page,title]
+        result3.extend([time, size, url])
+        self.done.emit(result3,self.title)
+
+
+
+class Workdownload(QThread):
+    """docstring for WorkThread"""
+    update = pyqtSignal(int,int)
+    para_update = pyqtSignal(int,str)
+    all_done = pyqtSignal()
+    error = pyqtSignal()
+    def __init__(self):
+        super(Workdownload, self).__init__()
+    def run(self):
+        self.before = 0
+        list_thread = []
+        for i in self.num:
+            title = self.list_[i][1].replace(" ","-")
+            url = self.list_[i][4] 
+            t1 = threading.Thread(target=self.download_thread,args=(url,title),name=str(i))
+            list_thread.append(t1)
+        
+        self.threadLock = threading.Lock()
+        for j in list_thread:
+            j.start()
+
+
+    def download_thread(self,url,title):
+        ssl._create_default_https_context = ssl._create_unverified_context
+        opener = urllib.request.build_opener()
+        opener.addheaders = [
+                ('Host', 'tx.acgvideo.com'),
+                ('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:56.0) Gecko/20100101 Firefox/56.0'),
+                ('Accept', '*/*'),
+                ('Accept-Language', 'en-US,en;q=0.5'),
+                ('Accept-Encoding', 'gzip, deflate, br'),
+                ('Range', 'bytes=0-'),  # Range 的值要为 bytes=0- 才能下载完整视频
+                ('Referer', 'https://www.bilibili.com/video/av14543079/'),
+                ('Origin', 'https://www.bilibili.com'),
+                ('Connection', 'keep-alive'),
+            ]
+        urllib.request.install_opener(opener)
+        for i in enumerate(url):
+            self.para_update.emit(int(threading.current_thread().name),str(i[0]+1))
+            if not os.path.exists(os.path.join(run_dir,self.title)+"/"+ title+"__"+str(i[0]+1)+'.flv'):
+                urllib.request.urlretrieve(i[1], filename=os.path.join(run_dir,self.title)+"/"+ title+"__"+str(i[0]+1)+'.flv', reporthook=self.report)
+            else:
+                print('已经存在!')
+
+        self.threadLock.acquire()
+        self.hecheng(title)
+        self.threadLock.release()
+
+    def hecheng(self,title):
+        os.chdir(self.title)
+        title_list = os.listdir('.')
+        title_list = [i.split("__")[0] for i in title_list]
+        c = Counter()
+        for ch in title_list:
+            c[ch] = c[ch] + 1
+
+        if c[title] != 1:
+            title_name_list = []    
+            for num in range(c[title]):
+                path_filename = title+'__'+str(num+1)
+                os.system('''ffmpeg -i %s.flv -c copy -bsf:v h264_mp4toannexb -f mpegts %s.ts'''% (path_filename, path_filename))
+                title_name_list.append(path_filename)
+
+            add_list = "|".join([j+'.ts' for j in title_name_list])
+            del_list = " ".join([j+'.ts' for j in title_name_list])
+            del2_list = " ".join([j+'.flv' for j in title_name_list])
+            os.system('''ffmpeg -i "concat:%s" -c copy -bsf:a aac_adtstoasc -movflags +faststart %s.mp4'''%(add_list,title))
+            os.system('''del %s %s''' % (del_list,del2_list))
+            print('''del %s %s''' % (del_list,del2_list))
+        elif c[title] == 1:    
+            os.system('''ffmpeg -i %s.flv -c copy -bsf:a aac_adtstoasc -movflags +faststart %s.mp4'''%(title+"__1",title))
+            os.system('''del %s.flv''' % (title+"__1"))
+        os.chdir(run_dir)
+
+    def report(self,count, blockSize, totalSize):
+        downloadedSize = count * blockSize
+        percent = int(downloadedSize * 100 / totalSize)
+        if not self.before == percent:
+            self.before = percent
+            self.update.emit(percent,int(threading.current_thread().name))
+        # sys.stdout.write(f"\rDownloaded: {downloadedSize} bytes, Total: {totalSize} bytes, {percent} % complete")
+        # sys.stdout.flush()
+
+if __name__ == '__main__':
+    run_dir = os.getcwd()
+    path = ''
+    app = QApplication(sys.argv)
+    mywin = MyMainWindow()
+    mywin.show()
+    sys.exit(app.exec())
---
+++ ui.py
@@ -0,0 +1,182 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'untitled.ui'
+#
+# Created by: PyQt5 UI code generator 5.10.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.setWindowModality(QtCore.Qt.NonModal)
+        MainWindow.resize(915, 580)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Maximum, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
+        MainWindow.setSizePolicy(sizePolicy)
+        MainWindow.setMaximumSize(QtCore.QSize(915, 580))
+        MainWindow.setFocusPolicy(QtCore.Qt.NoFocus)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(":/main_ico/bilibili.ico"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        MainWindow.setWindowIcon(icon)
+        MainWindow.setStyleSheet("border-image: url(:/main_ico/background.jpg);")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setMaximumSize(QtCore.QSize(915, 580))
+        self.centralwidget.setObjectName("centralwidget")
+        self.display_table = QtWidgets.QTableWidget(self.centralwidget)
+        self.display_table.setEnabled(True)
+        self.display_table.setGeometry(QtCore.QRect(20, 130, 881, 431))
+        font = QtGui.QFont()
+        font.setFamily("幼圆")
+        font.setPointSize(10)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(50)
+        self.display_table.setFont(font)
+        self.display_table.setAutoFillBackground(False)
+        self.display_table.setStyleSheet("border-image: none;\n"
+"background-color:rgba(0, 0, 0,0.3);\n"
+"font: 10pt \"幼圆\";\n"
+"\n"
+"selection-background-color: rgba(0, 0, 0,0.5);\n"
+"selection-color: rgb(255, 255, 255);\n"
+"gridline-color: white;\n"
+"color:white;\n"
+"border-radius:10px;")
+        self.display_table.setAutoScroll(True)
+        self.display_table.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+        self.display_table.setAlternatingRowColors(False)
+        self.display_table.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
+        self.display_table.setShowGrid(False)
+        self.display_table.setGridStyle(QtCore.Qt.SolidLine)
+        self.display_table.setObjectName("display_table")
+        self.display_table.setColumnCount(6)
+        self.display_table.setRowCount(0)
+        item = QtWidgets.QTableWidgetItem()
+        self.display_table.setHorizontalHeaderItem(0, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.display_table.setHorizontalHeaderItem(1, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.display_table.setHorizontalHeaderItem(2, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.display_table.setHorizontalHeaderItem(3, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.display_table.setHorizontalHeaderItem(4, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.display_table.setHorizontalHeaderItem(5, item)
+        self.display_table.horizontalHeader().setVisible(True)
+        self.display_table.horizontalHeader().setCascadingSectionResizes(False)
+        self.display_table.horizontalHeader().setHighlightSections(False)
+        self.display_table.horizontalHeader().setSortIndicatorShown(False)
+        self.display_table.verticalHeader().setVisible(False)
+        self.display_table.verticalHeader().setDefaultSectionSize(37)
+        self.display_table.verticalHeader().setHighlightSections(True)
+        self.display_table.verticalHeader().setSortIndicatorShown(False)
+        self.display_table.verticalHeader().setStretchLastSection(False)
+        self.url_edit = QtWidgets.QLineEdit(self.centralwidget)
+        self.url_edit.setGeometry(QtCore.QRect(90, 50, 511, 41))
+        self.url_edit.setStyleSheet("border-color: rgb(255, 255, 255);\n"
+"border-image:none;\n"
+"background-color:rgba(0, 0, 0,0.2);\n"
+"border:1px solid rgb(255, 255, 255);\n"
+"font: 10pt \"幼圆\";\n"
+"color:white;\n"
+"selection-background-color: rgba(0, 0, 0,0.5);\n"
+"border-radius:10px;")
+        self.url_edit.setObjectName("url_edit")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(650, 50, 93, 41))
+        self.pushButton.setStyleSheet("border-image:none;\n"
+"border:1px solid rgb(255, 255, 255);\n"
+"background-color:rgba(100, 100, 100,0.5);\n"
+"color:white;\n"
+"border-radius:10px;\n"
+"font: 17pt \"幼圆\";")
+        self.pushButton.setObjectName("pushButton")
+        self.all_select_check_button = QtWidgets.QCheckBox(self.centralwidget)
+        self.all_select_check_button.setGeometry(QtCore.QRect(-150, 100, 121, 21))
+        font = QtGui.QFont()
+        font.setFamily("幼圆")
+        font.setPointSize(12)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(50)
+        self.all_select_check_button.setFont(font)
+        self.all_select_check_button.setMouseTracking(True)
+        self.all_select_check_button.setToolTip("")
+        self.all_select_check_button.setStyleSheet("border-image:none;\n"
+"font: 12pt \"幼圆\";\n"
+"color:white;\n"
+"background-color:rgba(0, 0, 0,0.2);")
+        self.all_select_check_button.setObjectName("all_select_check_button")
+        self.download_button = QtWidgets.QPushButton(self.centralwidget)
+        self.download_button.setGeometry(QtCore.QRect(922, 50, 93, 41))
+        self.download_button.setStyleSheet("border-image:none;\n"
+"background-color:rgba(100, 100, 100,0.5);\n"
+"border:1px solid rgb(255, 255, 255);\n"
+"color:white;\n"
+"font: 17pt \"幼圆\";\n"
+"border-radius:10px;")
+        self.download_button.setObjectName("download_button")
+        self.close_button = QtWidgets.QPushButton(self.centralwidget)
+        self.close_button.setGeometry(QtCore.QRect(870, 10, 31, 31))
+        font = QtGui.QFont()
+        font.setFamily("幼圆")
+        font.setPointSize(17)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(50)
+        self.close_button.setFont(font)
+        self.close_button.setStyleSheet("border-image:none;\n"
+"border:1px solid rgb(255, 255, 255);\n"
+"background-color:rgba(255, 0, 0,0.5);\n"
+"color:white;\n"
+"border-radius:10px;\n"
+"font: 17pt \"幼圆\";")
+        self.close_button.setObjectName("close_button")
+        self.min_button = QtWidgets.QPushButton(self.centralwidget)
+        self.min_button.setGeometry(QtCore.QRect(830, 10, 31, 31))
+        self.min_button.setStyleSheet("border-image:none;\n"
+"border:1px solid rgb(255, 255, 255);\n"
+"background-color:rgba(0, 0, 255,0.5);\n"
+"color:white;\n"
+"border-radius:10px;\n"
+"font: 17pt \"幼圆\";")
+        self.min_button.setObjectName("min_button")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        self.pushButton.clicked.connect(MainWindow.jiexi)
+        self.all_select_check_button.stateChanged['int'].connect(MainWindow.all_select)
+        self.download_button.clicked.connect(MainWindow.download)
+        self.min_button.clicked.connect(MainWindow.min_click)
+        self.close_button.clicked.connect(MainWindow.close_click)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "bilibili-download"))
+        self.display_table.setSortingEnabled(False)
+        item = self.display_table.horizontalHeaderItem(0)
+        item.setText(_translate("MainWindow", "选择"))
+        item = self.display_table.horizontalHeaderItem(1)
+        item.setText(_translate("MainWindow", "标题"))
+        item = self.display_table.horizontalHeaderItem(2)
+        item.setText(_translate("MainWindow", "时长"))
+        item = self.display_table.horizontalHeaderItem(3)
+        item.setText(_translate("MainWindow", "大小"))
+        item = self.display_table.horizontalHeaderItem(4)
+        item.setText(_translate("MainWindow", "进度"))
+        item = self.display_table.horizontalHeaderItem(5)
+        item.setText(_translate("MainWindow", "段落"))
+        self.pushButton.setText(_translate("MainWindow", "确定"))
+        self.all_select_check_button.setText(_translate("MainWindow", "我全都要！"))
+        self.download_button.setText(_translate("MainWindow", "下载"))
+        self.close_button.setText(_translate("MainWindow", "×"))
+        self.min_button.setText(_translate("MainWindow", "-"))
+
+import bilibili_rc
